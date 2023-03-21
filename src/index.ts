fc4a678db22ef9ce6f6b3b4f87c0b65e795e9e4c
clemlak <clementlakhal@gmail.com>
2023-03-21 18:07:45 +0400

    feat: refactor some functions

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,16 @@ const CREATE_POOL = "0B";
 const CREATE_PAIR = "0C";
 const INSTRUCTION_JUMP = "AA";
 
-export function formatHex(input: BigNumber) {
+export function bigNumbertoHex(input: BigNumber): string {
   return input._hex.substring(2, input._hex.length);
 }
 
-export function toHex(input: number) {
-  return forceLeadingZero(input.toString(16));
-}
+export function int8ToHex(input: number) {
+  if (input > 0xff) {
+    throw new Error('Pointer too large');
+  }
 
-export function forceLeadingZero(input: string) {
-  return input.length % 2 === 0 ? input : `0${input}`;
+  return input.toString(16).padStart(2, '0');
 }
 
 export function packAmount(amount: BigNumber) {
@@ -29,13 +29,17 @@ export function packAmount(amount: BigNumber) {
     ++power;
   }
 
-  return `${toHex(power)}${formatHex(amount)}`;
+  return `${int8ToHex(power)}${bigNumbertoHex(amount)}`;
 }
 
 export function encodeCreatePair(
   token0: string,
   token1: string,
 ): string {
+  if (token0.length !== 42 || token1.length !== 42) {
+    throw new Error('Invalid token address');
+  }
+
   let token0Packed = token0.substring(2, token0.length);
   let token1Packed = token1.substring(2, token1.length);
 
@@ -43,7 +47,7 @@ export function encodeCreatePair(
 }
 
 export function encodeCreatePool(
-  pairId: string,
+  pairId: BigNumber,
   controller: string,
   priorityFee: BigNumber,
   fee: BigNumber,
@@ -53,17 +57,21 @@ export function encodeCreatePool(
   maxPrice: BigNumber,
   price: BigNumber
 ): string {
+  if (controller.length !== 42) {
+    throw new Error('Invalid controller address');
+  }
+
   const packedMaxPrice = packAmount(maxPrice);
 
   let data = `0x${CREATE_POOL}`;
-  data += pairId;
+  data += bigNumbertoHex(pairId).padStart(6, '0');
   data += controller.substring(2, controller.length);
-  data += formatHex(priorityFee);
-  data += formatHex(fee);
-  data += formatHex(vol);
-  data += formatHex(dur);
-  data += formatHex(jit);
-  data += toHex(36 + packedMaxPrice.length);
+  data += bigNumbertoHex(priorityFee).padStart(4, '0');
+  data += bigNumbertoHex(fee).padStart(4, '0');
+  data += bigNumbertoHex(vol).padStart(4, '0');
+  data += bigNumbertoHex(dur).padStart(4, '0');
+  data += bigNumbertoHex(jit).padStart(4, '0');
+  data += int8ToHex(35 + packedMaxPrice.length / 2);
   data += packedMaxPrice;
   data += packAmount(price);
 
@@ -73,18 +81,18 @@ export function encodeCreatePool(
 export function encodeAllocateOrDeallocate(
   shouldAllocate: boolean,
   useMax: boolean,
-  poolId: string,
+  poolId: BigNumber,
   amount: BigNumber,
 ): string {
   let data = `0x${useMax ? '1' : '0'}${shouldAllocate ? '1' : '3'}`;
-  data += poolId;
+  data += bigNumbertoHex(poolId).padStart(8, '0');
   data += packAmount(amount);
 
   return data;
 }
 
 export function encodeClaim(
-  poolId: string,
+  poolId: BigNumber,
   fee0: BigNumber,
   fee1: BigNumber,
 ): string {
@@ -92,12 +100,18 @@ export function encodeClaim(
   const fee1Packed = packAmount(fee1);
   const pointer = 1 + 8 + fee0Packed.length;
 
-  return `0x04${poolId}${toHex(pointer)}${fee0Packed}${fee1Packed}`;
+  let data = `0x${CLAIM}`;
+  data += bigNumbertoHex(poolId).padStart(8, '0');
+  data += int8ToHex(pointer);
+  data += fee0Packed;
+  data += fee1Packed;
+
+  return data;
 }
 
 export function encodeSwap(
   useMax: boolean,
-  poolId: string,
+  poolId: BigNumber,
   amount0: BigNumber,
   amount1: BigNumber,
   sellAsset: boolean,
@@ -105,8 +119,8 @@ export function encodeSwap(
   const amount0Packed = packAmount(amount0);
 
   let data = `0x${useMax ? '1' : '0'}${sellAsset ? '6' : '5'}`;
-  data += poolId;
-  data += toHex(1 + 8 + amount0Packed.length);
+  data += bigNumbertoHex(poolId).padStart(8, '0');
+  data += int8ToHex(1 + 8 + amount0Packed.length);
   data += amount0Packed;
   data += packAmount(amount1);
 
